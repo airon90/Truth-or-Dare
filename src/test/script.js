fef753307e779f4bc1c602145a8e3ab102e17f0b
Sylhare <sylhareng@gmail.com>
2017-06-15 20:20:12 -0400

    Initial commit
    Creation of the app and the bootstrap

---
+++ src/test/script.js
@@ -0,0 +1,9 @@
+//var myButton = document.getElementById("clickButton");
+myButton.addEventListener('click', doSomething, false)
+    //var myText = document.getElementById("helloText"); 
+
+function doSomething() {
+    //  myText.textContent = "hello, world!";
+    alert("hello, world!");
+
+}
